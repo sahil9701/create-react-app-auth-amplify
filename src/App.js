e1f663925c5f56adf549c67926386dc9dce5d6c7
Sahil Bhimjiani <46335522+sahil9701@users.noreply.github.com>
2021-05-06 15:38:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import {AmplifyAuthenticator, AmplifySignIn} from '@aws-amplify/ui-react'; 
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
-      <AmplifyAuthenticator usernameAlias="email">
-    <AmplifySignIn headerText="Sign In" slot="sign-in" hideSignUp></AmplifySignIn>
-    </AmplifyAuthenticator>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
